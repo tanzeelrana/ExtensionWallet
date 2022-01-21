3c725304f209e36abd00f8fac16f3e463013de01
Tanzeel Rana <tanzeelrana@cmail.carleton.ca>
2022-01-22 02:23:01 +0500

    Conditional rendering

--- src/components/Homepage-reg-email.tsx
+++ src/components/Homepage-reg-email.tsx
@@ -23,11 +23,14 @@ import {
     IonToolbar,
 } from '@ionic/react';
 
-import { arrowForward, closeCircleSharp, playForward, } from 'ionicons/icons';
+import { arrowForward } from 'ionicons/icons';
+import { useState } from 'react';
 import './Homepage-reg-email.css';
 
 
 const HomepageRegEmail: React.FC = () => {
+    const [emailActive, setEmailActive] = useState<boolean>(true);
+    const [phoneActive, setPhoneActive] = useState<boolean>(false);
 
     return (
         <IonPage>
@@ -39,35 +42,47 @@ const HomepageRegEmail: React.FC = () => {
             <IonContent>
                 <IonCardHeader>
                     <IonCard>
-                        <IonSegment >
-                            <IonSegment value ="email" onIonChange={e => console.log('Segment selected', e.detail.value)}>
-                                <IonSegmentButton value="email">
-                                    <IonLabel>Email</IonLabel>
-                                </IonSegmentButton>
-                                <IonSegmentButton value="phone">
-                                    <IonLabel>Phone</IonLabel>
-                                </IonSegmentButton>
-                            </IonSegment>
+                        <IonSegment value={emailActive ? "email" : "phone"}>
+                            <IonSegmentButton
+                                value="email"
+                                onClick={() => {
+                                    setPhoneActive(false);
+                                    setEmailActive(true);
+                                }}
+                            >
+                                <IonLabel>Email</IonLabel>
+                            </IonSegmentButton>
+                            <IonSegmentButton
+                                value="phone"
+                                onClick={() => {
+                                    setEmailActive(false);
+                                    setPhoneActive(true);
+                                }}
+                            >
+                                <IonLabel>Phone</IonLabel>
+                            </IonSegmentButton>
                         </IonSegment>
                     </IonCard>
                 </IonCardHeader>
                 <IonCard>
                     <IonCardContent>
-                        <IonInput placeholder=' Ex. (337) 378 8383' id="number" type='number'></IonInput>
+                        {emailActive ? <IonInput placeholder=' Ex. johndoe@gmail.com' id="email" type='text'></IonInput> : <IonInput placeholder=' Ex. (337) 378 8383' id="phone" type='number'></IonInput>}
                         <div className="center-button">
                             <IonButton size='default' color='medium'>Continue<IonIcon icon={arrowForward} slot="end" /></IonButton>
                         </div>
-                        <div style={{textAlign : "center", paddingTop: "5%"}}>
-                            <IonLabel><p> by clicking continue you must agree to near labs</p> </IonLabel>
-                            <div style={{textAlign : "center"}}>
-                            <IonRouterLink href="#"> Terms & Conditions </IonRouterLink>
-                            <IonLabel> and </IonLabel>
-                            <IonRouterLink href="#"> Privacy Policy </IonRouterLink>
-                            </div>
+                        <div style={{ textAlign: "center", paddingTop: "5%" }}>
+                            <p>
+                                <IonLabel> by clicking continue you must agree to near labs</IonLabel>
+                                <div style={{ textAlign: "center" }}>
+                                    <IonRouterLink href="#"> Terms & Conditions </IonRouterLink>
+                                    <IonLabel> and </IonLabel>
+                                    <IonRouterLink href="#"> Privacy Policy </IonRouterLink>
+                                </div>
+                            </p>
                         </div>
                         <div className="horizontal-line" />
                         <div className="center-button">
-                            <IonLabel color = "black"><p>Already have NEAR account?</p></IonLabel>
+                            <IonLabel color="black"><p>Already have NEAR account?</p></IonLabel>
                         </div>
                         <div className="center-button">
                             <IonButton id="button" size='default' color='dark'>Log in with NEAR<IonIcon icon={arrowForward} slot="end" /></IonButton>
