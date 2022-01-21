8b6b24c5a2b996ac68ea57abb4bc6e7bfe59fb3e
aalishaslam <ali.aslam@devflovv.com>
2022-01-21 23:47:58 +0500

    secure account

---
+++ src/components/SecureAccount.tsx
@@ -0,0 +1,67 @@
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonContent,
+  IonFooter,
+  IonHeader,
+  IonIcon,
+  IonInput,
+  IonLabel,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
+
+import { arrowForward, closeCircleSharp, } from 'ionicons/icons';
+import './SecureAccount.css';
+
+const SecureAccount: React.FC = () => {
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar >
+          <IonTitle className="center" >Create NEAR account</IonTitle>
+          <IonIcon size='large' slot='end' icon={closeCircleSharp}></IonIcon>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardSubtitle>Enter an Account ID to use with your NEAR account. Your Account ID will be used for all NEAR operations, including sending and receiving assets.
+            </IonCardSubtitle>
+          </IonCardHeader>
+          <IonCardContent>
+
+            <IonLabel>FullName</IonLabel>
+            <IonInput placeholder='Ex. John doe' id="text" type='password'></IonInput>
+            <br />
+            <IonLabel>Account ID</IonLabel>
+            <IonInput placeholder="yourname" id="text" type='password'></IonInput>
+            <br />
+            <div className="center">
+              <IonButton size='default' color='medium'>Continue<IonIcon icon={arrowForward} slot="end" /></IonButton>
+              <br />
+              <br />
+              <IonLabel>By creating a NEAR account, you agree to the NEAR </IonLabel>
+              <IonLabel>Wallet <a href="/">Terms of Service</a> and <a href="/">Privacy Policy</a>.</IonLabel>
+            </div>
+          </IonCardContent>
+          <IonFooter>
+            <br />
+            <div className="center">
+              <IonLabel><b>Already have NEAR account?</b></IonLabel>
+              <IonButton id="button" size='default' color='dark'>Log in with NEAR<IonIcon icon={arrowForward} slot="end" /></IonButton>
+            </div>
+            <br />
+          </IonFooter>
+        </IonCard>
+      </IonContent>
+    </IonPage >
+  );
+};
+export default SecureAccount;
+
