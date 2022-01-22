f50e35e5ca9a0885617af3c701d51b758afecda7
Tanzeel Rana <tanzeelrana@cmail.carleton.ca>
2022-01-22 12:50:59 +0500

    Clean up some code

--- src/components/WelcomeBack.tsx
+++ src/components/WelcomeBack.tsx
@@ -1,28 +1,15 @@
 import {
     IonButton,
-    IonCard,
-    IonCardContent,
-    IonCardHeader,
     IonContent,
-    IonGrid,
     IonHeader,
     IonIcon,
     IonImg,
     IonInput,
     IonLabel,
     IonPage,
-    IonRouterLink,
-    IonSegment,
-    IonSegmentButton,
-    IonTitle,
-    IonToolbar,
 } from '@ionic/react';
-import { IonContext } from '@ionic/react/dist/types/contexts/IonContext';
-
 import { arrowForward } from 'ionicons/icons';
-import { useState } from 'react';
 import './Homepage-reg-email.css';
-
 
 const WelcomeBack: React.FC = () => {
     return (
