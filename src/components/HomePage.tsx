b568c01889d780a67711b4a57bf4044a05795d91
Tanzeel Rana <tanzeelrana@cmail.carleton.ca>
2022-01-22 06:08:57 -0500

    added ui code for home screen

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -17,6 +17,9 @@ import {
     IonInput,
     IonItem,
     IonLabel,
+    IonList,
+    IonListHeader,
+    IonNote,
     IonPage,
     IonRouterLink,
     IonRow,
@@ -24,30 +27,17 @@ import {
     IonSegmentButton,
     IonSelect,
     IonSelectOption,
+    IonTab,
+    IonTabBar,
+    IonTabButton,
+    IonThumbnail,
     IonTitle,
     IonToolbar,
 } from '@ionic/react';
 
-import { arrowForward, caretDownCircleOutline, cogOutline, notificationsOutline, personCircle, search } from 'ionicons/icons';
+import { arrowForward, caretDownCircleOutline, chevronForwardCircleOutline, chevronForwardOutline, cogOutline, notificationsOutline, personCircle, search } from 'ionicons/icons';
 import { useState } from 'react';
 import './HomePage.css';
-
-{/* <ion-toolbar>
-  <ion-buttons slot="secondary">
-    <ion-button>
-      <ion-icon slot="icon-only" name="person-circle"></ion-icon>
-    </ion-button>
-    <ion-button>
-      <ion-icon slot="icon-only" name="search"></ion-icon>
-    </ion-button>
-  </ion-buttons>
-  <ion-buttons slot="primary">
-    <ion-button color="secondary">
-      <ion-icon slot="icon-only" ios="ellipsis-horizontal" md="ellipsis-vertical"></ion-icon>
-    </ion-button>
-  </ion-buttons>
-  <ion-title>Default Buttons</ion-title>
-</ion-toolbar> */}
 
 const HomePage: React.FC = () => {
     return (
@@ -56,9 +46,9 @@ const HomePage: React.FC = () => {
                 <IonToolbar >
                     <IonButtons slot="secondary">
                         <IonButton>
-                            <IonAvatar>
+                            <IonThumbnail>
                                 <img src="assets/Group397.png" />
-                            </IonAvatar>
+                            </IonThumbnail>
                         </IonButton>
                     </IonButtons>
                     <IonButtons slot="primary">
@@ -70,7 +60,7 @@ const HomePage: React.FC = () => {
                         </IonButton>
                     </IonButtons>
                     <IonChip outline color="dark">
-                        <IonIcon icon='caret-down-outline'></IonIcon>
+                        <IonIcon icon={cogOutline}></IonIcon>
                         <IonLabel>JohnDoe.near</IonLabel>
                         <IonIcon icon={caretDownCircleOutline}></IonIcon>
                     </IonChip>
@@ -80,31 +70,48 @@ const HomePage: React.FC = () => {
                 <IonGrid>
                     <IonRow>
                         <IonCol>
-                            <IonCard>
+                            <IonCard style={{ "background-color": "#EAEFFF" }}>
                                 <IonCardHeader>
-                                    <IonCardSubtitle>Contacts</IonCardSubtitle>
+                                    <IonItem style={{ "background-color": "#EAEFFF" }}>
+                                        <IonLabel>Contacts</IonLabel>
+                                        <IonIcon icon={chevronForwardOutline} slot="end" />
+                                    </IonItem>
                                 </IonCardHeader>
                                 <IonCardContent>
-
+                                    <IonThumbnail>
+                                        <img src="assets/Ellipse48.png" />
+                                    </IonThumbnail>
                                 </IonCardContent>
                             </IonCard>
                         </IonCol>
                         <IonCol>
-                            <IonCard>
+                            <IonCard style={{ "background-color": "#885FFF" }}>
                                 <IonCardHeader>
-                                    <IonCardSubtitle>web3 Apps</IonCardSubtitle>
+                                    <IonItem style={{ "background-color": "#885FFF" }}>
+                                        <IonLabel>web3 Apps</IonLabel>
+                                        <IonIcon icon={chevronForwardOutline} slot="end" />
+                                    </IonItem>
                                 </IonCardHeader>
                                 <IonCardContent>
-
+                                    <IonButton color="tertiary" style={{ "color": '#18D7A9' }}>12 Connected</IonButton>
                                 </IonCardContent>
                             </IonCard>
                         </IonCol>
                     </IonRow>
                     <IonRow>
                         <IonCol>
-                            <IonCard>
+                            <IonCard style={{ "background-color": "#F7E9FF" }}>
                                 <IonCardHeader>
-                                    <IonCardSubtitle>Start creating Your NFT Today</IonCardSubtitle>
+                                    <IonGrid>
+                                        <IonRow>
+                                            <IonCol size="4">
+                                                <IonCardTitle>Start creating Your NFT Today</IonCardTitle>
+                                            </IonCol>
+                                            <IonCol size="8" className='center'>
+                                                <img src="assets/Group339.png" />
+                                            </IonCol>
+                                        </IonRow>
+                                    </IonGrid>
                                 </IonCardHeader>
                                 <IonCardContent>
                                     <IonButton size='default' color='primary'>Create NFT<IonIcon icon={arrowForward} slot="end" /></IonButton>
@@ -112,6 +119,53 @@ const HomePage: React.FC = () => {
                             </IonCard>
                         </IonCol>
                     </IonRow>
+                    <IonRow className='center'>
+                        <IonCol>
+                            <IonSegment>
+                                <IonSegmentButton value="collectibles">
+                                    <IonLabel>Collectibles</IonLabel>
+                                </IonSegmentButton>
+                                <IonSegmentButton value="transactions">
+                                    <IonLabel>Transactions</IonLabel>
+                                </IonSegmentButton>
+                            </IonSegment>
+                        </IonCol>
+                    </IonRow>
+
+                    <IonList>
+                        <IonItem>
+                            <IonThumbnail slot="start">
+                                <img src="assets/Rectangle228.png" />
+                            </IonThumbnail>
+                            <IonLabel>
+                                <h2>Digital Ninga</h2>
+                                <h3>by johndoe.near</h3>
+                            </IonLabel>
+                            <IonIcon icon={chevronForwardOutline} slot="end" />
+                        </IonItem>
+
+                        <IonItem>
+                            <IonThumbnail slot="start">
+                                <img src="assets/Rectangle228.png" />
+                            </IonThumbnail>
+                            <IonLabel>
+                                <h2>Digital Ninga</h2>
+                                <h3>by johndoe.near</h3>
+                            </IonLabel>
+                            <IonIcon icon={chevronForwardOutline} slot="end" />
+                        </IonItem>
+
+                        <IonItem>
+                            <IonThumbnail slot="start">
+                                <img src="assets/Rectangle228.png" />
+                            </IonThumbnail>
+                            <IonLabel>
+                                <h2>Digital Ninga</h2>
+                                <h3>by johndoe.near</h3>
+                            </IonLabel>
+                            <IonIcon icon={chevronForwardOutline} slot="end" />
+                        </IonItem>
+                    </IonList>
                 </IonGrid>
             </IonContent>
         </IonPage >
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -55,8 +55,6 @@ const appPages: AppPage[] = [
   }
 ];
 
-const labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-
 const Menu: React.FC = () => {
   const location = useLocation();
 
@@ -77,16 +75,6 @@ const Menu: React.FC = () => {
               </IonMenuToggle>
             );
           })}
-        </IonList>
-
-        <IonList id="labels-list">
-          <IonListHeader>Labels</IonListHeader>
-          {labels.map((label, index) => (
-            <IonItem lines="none" key={index}>
-              <IonIcon slot="start" icon={bookmarkOutline} />
-              <IonLabel>{label}</IonLabel>
-            </IonItem>
-          ))}
         </IonList>
       </IonContent>
     </IonMenu>
